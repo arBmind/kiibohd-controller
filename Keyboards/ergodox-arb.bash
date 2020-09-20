0ef6365815403451a0214b702f51e49ec40e3059
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-09-20 16:15:03 +0200

    added all customizations
    * upgraded kll files to current version

---
+++ Keyboards/ergodox-arb.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Keyboard: Ergodox
+#
+# These build scripts are just a convenience for configuring your keyboard (less daunting than CMake)
+# Jacob Alexander 2015-2019
+
+
+# Build the Left Side
+"${BASH_SOURCE%/*}/ergodox-l-arb.bash"
+
+# Build the Right Side
+"${BASH_SOURCE%/*}/ergodox-r-arb.bash"
